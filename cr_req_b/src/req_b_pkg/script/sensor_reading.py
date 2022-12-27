70a709304f77d47e57a3796cf2c484da5f31cec5
mohamed99akram <mohamed1999akram@gmail.com>
2022-12-27 12:29:01 +0200

    added sensor readings messages

--- cr_req_b/src/req_b_pkg/script/sensor_reading.py
+++ cr_req_b/src/req_b_pkg/script/sensor_reading.py
@@ -25,16 +25,39 @@ def odom_callback(msg):
 
 def hub_callback(front, rear, odom):
     # print min and max of front and rear laser in deg
-    print('front: ', np.rad2deg(front.angle_min), np.rad2deg(front.angle_max))
-    print('rear: ', np.rad2deg(rear.angle_min), np.rad2deg(rear.angle_max))
+    # print('front: ', np.rad2deg(front.angle_min), np.rad2deg(front.angle_max))
+    # print('rear: ', np.rad2deg(rear.angle_min), np.rad2deg(rear.angle_max))
+    # print('front: ', (front.angle_min), (front.angle_max))
+    # print('rear: ', (rear.angle_min), (rear.angle_max))
     msggg = HeaderAndReading()
     msggg.pose = odom.pose
     msggg.twist = odom.twist
-    # TODO fill in angles and ranges
+    # Front
+    front_angles_readings = list(zip(np.arange(front.angle_min, front.angle_max+front.angle_increment, front.angle_increment), front.ranges))
+    # print("front1: ", [(np.rad2deg(a),b) for (a,b) in front_angles_readings])
+    # take -90 to 90
+    front_angles_readings = list(filter(lambda x: np.rad2deg(x[0]) >= -90 and np.rad2deg(x[0]) < 90, front_angles_readings))
+    # print("front2: ", [(np.rad2deg(a),b) for (a,b) in front_angles_readings])
+    # Rear
+    rear_angles_readings = list(zip(np.arange(rear.angle_min, rear.angle_max+rear.angle_increment, rear.angle_increment), rear.ranges))
+    # print('rear1: ', [(np.rad2deg(a), b) for (a,b) in rear_angles_readings])
+    # take -90 to 90
+    rear_angles_readings = list(filter(lambda x: np.rad2deg(x[0]) >= -90 and np.rad2deg(x[0])< 90, rear_angles_readings))
+    # print('rear2: ', [(np.rad2deg(a), b) for (a,b) in rear_angles_readings])
+    # [-135:135] -> [-45:45]
+    rear_angles_readings = list(map(lambda x: (x[0] + (np.deg2rad(180) if x[0] < np.deg2rad(0) else np.deg2rad(-180)), x[1]), rear_angles_readings))
+    # print('rear3: ', [(np.rad2deg(a), b) for (a,b) in rear_angles_readings])
+    all_readings = sorted(front_angles_readings + rear_angles_readings)
 
+    angles2, ranges2 = zip(*all_readings)
+    msggg.angles = angles2
+    msggg.sensors_data = ranges2
+    # print('PRODUCED: ', list(zip(np.rad2deg(angles2), ranges2)))
+    # print(len(all_readings))
     # publish msggg to sensor_topic
     pub = rospy.Publisher('/sensor_topic', HeaderAndReading, queue_size=10)
     pub.publish(msggg)
+    print('/sensor_topic written')
     # print('hub: ', front, rear, odom)
 
 
